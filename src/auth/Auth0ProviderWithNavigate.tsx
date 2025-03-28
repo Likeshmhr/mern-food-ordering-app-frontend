94795ddbe8786042d796c6aa39341e5447ba5c24
likeshmhr <likeshmhz1213@gmail.com>
2025-03-28 18:20:57 +0545

    added checkout flow

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -5,7 +5,7 @@ type Props = {
     children: React.ReactNode;
 };
 
-import { Auth0Provider} from "@auth0/auth0-react";
+import { AppState, Auth0Provider} from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 const Auth0ProviderWithNavigate = ({children }:Props) => {
@@ -18,8 +18,8 @@ const Auth0ProviderWithNavigate = ({children }:Props) => {
     if(!domain || !clientId || !redirectUri || !audience){
         throw new Error("unable to initialise auth");
     }
-    const onRedirectCallback = async()=> {
-        navigate("/auth-callback")
+    const onRedirectCallback = (appState?: AppState)=> {
+        navigate(appState?.returnTo || "/auth-callback")
         
     };
 
